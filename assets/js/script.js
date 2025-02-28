71e8dc728d883302a32c98e7b68c123ab643e80c
Ingrid Oliveira <ngrid.vianna1@outlook.com>
2025-02-28 16:01:03 -0300

    :wrench: config js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,12 @@
-console.log("teste");
+let x = document.querySelector(".x");
+let o = document.querySelector(".o");
+let boxes = document.querySelectorAll(".box");
+let buttons = document.querySelectorAll("#buttons-container button");
+let messageContainer = document.querySelector("message");
+let messageText = document.querySelector("message p");
+let secondPlayer;
+
+// contador de jogadas
+
+let player1 = 0;
+let player2 = 0;

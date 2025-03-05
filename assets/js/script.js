9177aaa07e57d0c6897c2086c0b093e8fa5d6472
Ingrid Oliveira <ngrid.vianna1@outlook.com>
2025-03-05 14:08:42 -0300

    :construction: fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@
 let x = document.querySelector('.x');
 let o = document.querySelector('.o');
 let boxes = document.querySelectorAll('.box');
-let buttons = document.querySelectorAll('#btn-container button');
+let buttons = document.querySelectorAll('#buttons-container');
 let messageContainer = document.querySelector('#message');
 let messageText = document.querySelector('#message p');
 let secondPlayer;
